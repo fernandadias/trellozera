a16e8f48f32fd41e7e1f9ef6cd37319813b42ef5
Nanda Dias <fernanda.dias@konduto.com>
2020-08-02 03:33:57 -0300

    Add styles for header filters

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiCircle, FiCheckCircle } from 'react-icons/fi';
 import LogoSrc from '../../assets/trellozera-logo.png';
 
 import * as S from './style';
@@ -10,9 +10,21 @@ function Header() {
   return (
     <S.HeaderContainer>
       <S.Logo className="Logo" src={LogoSrc} alt="Dois retângulos verdes com o texto 'Trellozera' " title="Trellozera | Meu repositório de videos maneiros" />
+      <S.Filters>
+        <span>filtrar por: </span>
+        <a href="$" className="active">todos</a>
+        <a href="$">
+          <FiCircle size={16} />
+          <span>vistos</span>
+        </a>
+        <a href="$">
+          <FiCheckCircle size={16} />
+          <span> não vistos</span>
+        </a>
+      </S.Filters>
       <S.Button>
         <FiPlus size={16} />
-        <span>Adicionar video</span>
+        <span>Novo video</span>
       </S.Button>
     </S.HeaderContainer>
   );
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -13,11 +13,44 @@ export const Logo = styled.img`
   height: 30px;
 `;
 
+export const Filters = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+  color: var(--subTitles);
+  font-variant: small-caps;
+  display: flex;
+  line-height: 32px;
+
+  > span{
+    margin-right: 16px;
+  }
+
+  a{
+    font-weight: 700;
+    color: var(--subTitles);
+    margin: 0 8px;
+    text-decoration: none;
+    line-height: 32px;
+    display: flex;
+    transition: color .2s ease-in-out;
+
+    svg{
+      height: 32px;
+      margin-right: 4px;
+    }
+
+    
+    &.active, &:hover{
+      color: var(--themeColor);
+    }
+  }
+`;
+
 export const Button = styled.button`
   border: 0;
   height: 36px;
-  padding: 8px;
-  background-color: var(--backColor);
+  padding: 8px 16px;
+  background-color: var(--themeColor);
   border-radius: 4px;
   font-family: Montserrat;
   font-style: normal;
@@ -25,6 +58,13 @@ export const Button = styled.button`
   font-size: 12px;
   color: var(--shape);
   display: flex;
+  letter-spacing: 0.08em;
+  transition: all .2s ease-in-out;
+
+  &:hover{
+    box-shadow: 0px 0px 40px rgba(11, 191, 148, 0.4);
+    opacity: .9;
+  }
 
   svg {
     margin-right: 8px;
